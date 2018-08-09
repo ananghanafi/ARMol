837c1928cd2f5e1b9c04e45fdbdf8ba6979ed178
zSANSANz <sandipermanasoebagio@gmail.com>
2018-08-09 15:28:26 +0700

    Ocr

--- app/src/main/java/com/masbie/armol/MainActivity.java
+++ app/src/main/java/com/masbie/armol/MainActivity.java
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -111,7 +111,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 statusMessage.setText(String.format(getString(R.string.ocr_error),
                         CommonStatusCodes.getStatusCodeString(resultCode)));
             }
-         
+
         }
         else {
             super.onActivityResult(requestCode, resultCode, data);
